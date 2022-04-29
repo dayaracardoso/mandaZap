37c3eaa0238a452fecd8675a08c51cfea33d7458
Dayara Rossi <100045697+dayaracardoso@users.noreply.github.com>
2022-04-29 16:44:48 -0300

    Adicionando arquivos

---
+++ script.js
@@ -0,0 +1,8 @@
+﻿function mandaZap(elemento) {
+    let texto = elemento.firstElementChild.innerText;
+    let numero = parseInt(prompt("Qual o número? (Digite apenas números)"));
+    let zapLink = `https://api.whatsapp.com/send?phone=55${numero}&text=${texto}`;
+    if(numero) {
+        window.open(zapLink);
+    }
+}
